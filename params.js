3ef292d27cbfcc05ad43049bec4cd63d457cad62
dung.nguyen5 <dung.nguyen5@be.com.vn>
2023-11-17 18:53:14 +0700

    first commit

---
+++ params.js
@@ -0,0 +1,15 @@
+const array1 = [
+  { key: "1", value: 1 },
+  { key: "1", value: 2 },
+  { key: "2", value: 3 },
+];
+
+const sum = array1.reduce((prev, cur) => {}, []);
+
+[
+  [
+    { key: "1", value: 1 },
+    { key: "1", value: 2 },
+  ],
+  [{ key: "2", value: 3 }],
+];
